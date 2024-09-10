49738481a64f04370b01bea2bcb0f51c1a9ac464
Cristiano Leal <cristianofran@hotmail.com>
2024-09-10 15:36:16 -0300

    Update Customer.cs

--- src/ValidationAttributes.WebApi/Models/Customer.cs
+++ src/ValidationAttributes.WebApi/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using ValidationAttributes.WebApi.DataAnntations;
+using System.ComponentModel.DataAnnotations;
+using ValidationAttributes.WebApi.DataAnntations;
 
 namespace ValidationAttributes.WebApi.Models
 {
@@ -6,9 +7,13 @@ namespace ValidationAttributes.WebApi.Models
 	{
 		public string Name { get; set; }
 		
+		[Required]
 		public string Email { get; set; }
 
-		[CustomerDateOfBirthValidation]
+		[DateOfBirthValidation]
 		public DateTime DateOfBirth { get; set; }
+
+		[CityValidation]
+		public string City { get; set; }
 	}
 }
